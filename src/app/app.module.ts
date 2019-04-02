1124272e5da470c74fdba71cf896f6ff96ffc4d5
adriangarciag <ad_j_gg@hotmail.es>
2019-04-02 10:48:27 -0600

    final

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,19 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { VoterComponent } from './voter/voter.component';
+import { TodosComponent } from './todos/todos.component';
+import { UsersComponent } from './users/users.component'; 
+import { UserDetailsComponent } from './user-details/user-details.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    VoterComponent,
+    TodosComponent,
+    UsersComponent,
+    UserDetailsComponent
   ],
   imports: [
     BrowserModule
