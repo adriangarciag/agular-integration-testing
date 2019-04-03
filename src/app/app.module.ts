81c2995ba2f11d8ae436bd10851c31626e78bbf3
adriangarciag <ad_j_gg@hotmail.es>
2019-04-02 18:02:53 -0600

    sada

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,10 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-import { VoterComponent } from './voter/voter.component';
-import { TodosComponent } from './todos/todos.component';
-import { UsersComponent } from './users/users.component'; 
-import { UserDetailsComponent } from './user-details/user-details.component';
+import { VoterComponent } from 'src/app/voter/voter.component';
+import { TodosComponent } from 'src/app/todos/todos.component';
+import { UsersComponent } from 'src/app/users/users.component'; 
+import { UserDetailsComponent } from 'src/app/user-details/user-details.component';
 
 
 @NgModule({
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,8 @@
 
-import { VoterComponent } from './voter/voter.component';
-import { TodosComponent } from './todos/todos.component';
-import { UsersComponent } from './users/users.component'; 
-import { UserDetailsComponent } from './user-details/user-details.component';
+import { VoterComponent } from 'src/app/voter/voter.component';
+import { TodosComponent } from 'src/app/todos/todos.component';
+import { UsersComponent } from 'src/app/users/users.component'; 
+import { UserDetailsComponent } from 'src/app/user-details/user-details.component';
 
 export const routes = [
   { path: 'users/:id', component: UserDetailsComponent },
